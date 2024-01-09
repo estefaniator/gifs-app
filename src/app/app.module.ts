58b1e7064b21e0991d55e52dcb4750494e7f36eb
Estefania Torres <estefaniatorresrico@gmail.com>
2024-01-09 15:51:46 +0100

    gifs-app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
+import { SharedModule } from "./shared/shared.module";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+
+
+  ],
+  providers: [
+
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
